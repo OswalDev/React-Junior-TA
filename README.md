c66eaf025cf89afe3ccf2a27373cd448fdaa2ae6
Oswaldo Soto <oswaldojsotog@gmail.com>
2023-04-19 16:57:13 -0400

    Catching API & string manipulation

---
+++ README.md
@@ -0,0 +1,7 @@
+React Live Coding Tecnical assessment for Juniors/Trainee
+
+APIs:
+- Facts Random: https://catfact.ninja/fact
+- Image Random: https://cataas.com/cat/says/hello
+
+Return a random cat fact from the first API and a picture from second API using the first word of the fact.
--- react-junior-ta/src/App.jsx
+++ react-junior-ta/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `/cat/says/:text?size=:size&color=:color
+// /c/s/:text?s=:size&c=:color`
+
+// You're not allowed to use Axios, React Query, SWR, Apollo
 function App() {
+  const [fact, setFact] = useState('')
+  const [image, setImage] = useState('')
+
+  useEffect( () => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+    .then(res => res.json())
+    .then(data => {
+      const { fact } = data
+      setFact(fact)
+
+      const firstWord = fact.split(' ')[0]
+    })
+  }, [])
+
+  async function getImageResponse() {
+    var str = fact.fact;
+    const response = await fetch('https://cataas.com/cat/says/:'+{str})
+    console.log(response)
+    return response
+  }
 
   return (
     <div className="App"> 
-      test
+    <h1>Cat's app</h1>
+      {fact && <p>{fact}</p>}
+      <img src={image.url} alt=""/>
     </div>
   )
 }
